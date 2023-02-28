ab135bb44492538778b5f6f2080ac48eca4c9cc3
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2023-02-28 14:34:26 +0600

    navbar account section

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,18 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useAuth } from "../contexts/AuthProvaider";
 import "../styles/Account.css";
 const Account = () => {
+  const { currentUser, logout } = useAuth();
+
   return (
     <div className="account">
-      <span className="material-icons-outlined" title="Account">
-        account_circle
-      </span>
-      <Link to="/signup">Signup</Link>
-      <Link to="/login">Login</Link>
-      {/* <span className="material-icons-outlined" title="Logout">
-        {" "}
-        logout{" "}
-      </span> */}
+      {currentUser ? (
+        <>
+          <span className="material-icons-outlined" title="Account">
+            account_circle
+          </span>
+          <span>{currentUser.displayName}</span>
+          <span
+            onClick={logout}
+            className="material-icons-outlined"
+            title="Logout"
+          >
+            {" "}
+            logout{" "}
+          </span>
+        </>
+      ) : (
+        <>
+          <Link to="/signup">Signup</Link>
+          <Link to="/login">Login</Link>
+        </>
+      )}
     </div>
   );
 };
